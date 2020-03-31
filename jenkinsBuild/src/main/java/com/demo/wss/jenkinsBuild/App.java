b0b630b55247e16065c564de24eba894a35ecffb
Nitin <Nshirke007@gmail.com>
2020-03-31 13:45:23 +0530

    initial commit for jenkins build

---
+++ jenkinsBuild/src/main/java/com/demo/wss/jenkinsBuild/App.java
@@ -0,0 +1,13 @@
+package com.demo.wss.jenkinsBuild;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
